4bf18b75f312f831db7f56f86a3bb7415a1d0a7d
wwwblogger1 <wwwblogger1@gmail.com>
2021-07-18 18:51:27 +0300

    go WebServer

---
+++ AppDatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServer.Models;
+
+namespace WebServer
+{
+    public class AppDatabaseContext: DbContext
+    {
+       
+
+        DbSet<User>Users { get; set; }
+    }
+
+
+    /*
+     * Любая ORM должна иметь контекст и классы
+     * Классы должны соответствовать таблицам
+     * 
+     * 
+     * Context
+     * Classes
+     *   User - UserTable
+     *   Manager - ManagerTable
+     */
+
+}
